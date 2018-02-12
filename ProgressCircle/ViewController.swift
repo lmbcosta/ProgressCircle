27cd2deae07f98123e8db412e179c39e7f45079b
lmbcosta <lmbcosta@hotmail.com>
2018-02-12 23:50:14 +0000

    Initial Commit

---
+++ ProgressCircle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProgressCircle
+//
+//  Created by Luis  Costa on 12/02/18.
+//  Copyright © 2018 Luis  Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
